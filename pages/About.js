ab912666abb2618fa07fa4abdbe27000b10a7a3a
chinocejas <nicocejas@outlook.com>
2022-10-22 21:36:24 -0300

    adding card page (#3)

---
+++ pages/About.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <div className="container">
+      <Head>
+        <title>chino.cejas</title>
+      </Head>
+
+      <main>
+        <h1>this is </h1>
+      </main>
+    </div>
+  )
+}
